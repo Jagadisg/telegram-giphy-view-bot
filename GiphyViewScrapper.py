3fa2293bf70c475a26f691d67a427ef95dde82e5
jagadish <jagadish.nadar41@gmail.com>
2024-04-16 20:39:45 +0530

    telgram_bot updated

--- GiphyViewScrapper.py
+++ GiphyViewScrapper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
 
 
 def get_driver():
@@ -20,14 +21,31 @@ def get_driver():
     
 
 def get_giphy_views(url):
+    views_count = 0
     try:
         driver = get_driver()
         driver.get(url)
-        if "media" in url:
-            wait = WebDriverWait(driver, 30)
-            media = wait.until(EC.presence_of_element_located((By.CLASS_NAME,"media_gif__MBeQG")))
-            media.click()
-        views_count = driver.find_element(By.CLASS_NAME,"ViewCountContainer-sc-15ri43l").text
+        if "media" in url and "giphy" in url:
+            try:
+                wait = WebDriverWait(driver, 30)
+                media = wait.until(EC.presence_of_element_located((By.CLASS_NAME,"media_gif__MBeQG")))
+                media.click()
+            except NoSuchElementException as e:
+                logger.error("NoSuchElementException occurred: {}".format(e))
+                return "Not a valid link."
+            except Exception as e:
+                logger.error(e)
+        try:
+            if "giphy" in url:
+                views_count = driver.find_element(By.CLASS_NAME,"ViewCountContainer-sc-15ri43l").text
+            else:
+                return "Invalid url"
+        except NoSuchElementException as e:
+            logger.error("NoSuchElementException occurred: {}".format(e))
+            return views_count
+        except Exception as e:
+            logger.error(e)
+            return views_count
         logger.info(views_count)
         driver.quit()
         return views_count
